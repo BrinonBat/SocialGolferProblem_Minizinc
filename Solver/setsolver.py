bffbdf01da25999043aff3ad2977d754949d4f0a
Matthew Coyle <matthew@coyle.fr>
2022-02-20 18:41:40 +0100

    possibly fixed arg order

--- Solver/setsolver.py
+++ Solver/setsolver.py
@@ -115,9 +115,9 @@ def Launch_Solver(sets,constraints, une_solution=False): # FlatBat
 			print("unnamed constraint")
 			return
 		elif c[0]=="union":
-			cstrobjs.append(Union(get_num_var(setobjs,c[1]),get_num_var(setobjs,c[2]),get_num_var(setobjs,c[3]),0))
+			cstrobjs.append(Union(get_num_var(setobjs,c[3]),get_num_var(setobjs,c[1]),get_num_var(setobjs,c[2]),0))
 		elif c[0]=="intersect":
-			cstrobjs.append(Intersection(get_num_var(setobjs,c[1]),get_num_var(setobjs,c[2]),get_num_var(setobjs,c[3]),1))
+			cstrobjs.append(Intersection(get_num_var(setobjs,c[3]),get_num_var(setobjs,c[1]),get_num_var(setobjs,c[2]),1))
 		elif c[0]=="cardeq":
 			cstrobjs.append(CardinalityEQ(get_num_var(setobjs,c[1]),get_num_var(setobjs,c[2]),0))
 		elif c[0]=="cardlt":
@@ -148,78 +148,78 @@ def Launch_Solver(sets,constraints, une_solution=False): # FlatBat
 
 if __name__ == "__main__":
 	import time
-
-	Ens=[
-		# ["I",set({1,2,3,4}),True],
-		["J",set(list(range(10))),False],
-		# ["D",set({1,3}),True],
 		# ["B",set({2,4}),True],
-		["E",set({3,4,5,6}),True],
-
-		# ["O",set({1,2,3,4}),False],
+		# ["I",set({1,2,3,4}),True],
+		# ["D",set({1,3}),True],
 		# ["OO",set({1,2,3,4}),False],
-		# ["2",int(2),True],
-		# ["3",int(3),True]
-	]
-
-	Cstr=[
-
 		# ["union","I","D","B"],
-		["equals","J","E"],
-		# ["cardlt","O","3"],
-		# ["existsIn","2","O"],
-	]
-
 
 	# Ens=[
-	# ["P",set({1,2,3,4}),True],
+	# 	["J",set(list(range(10))),False],
+	# 	["E",set({3,4,5,6}),True],
+	# 	["O",set({1,2,3,4}),False],
+	# 	["2",int(2),True],
+	# 	["3",int(3),True]
 
-	# ["2",int(2),True],
-
-	# ["G11",set({1,2,3,4}),False],
-	# ["G12",set({1,2,3,4}),False],
-	# ["G21",set({1,2,3,4}),False],
-	# ["G22",set({1,2,3,4}),False],
-
-
-	# ["G11nG12",set({1,2,3,4}),False],
-	# ["G11nG21",set({1,2,3,4}),False],
-	# ["G21nG22",set({1,2,3,4}),False],
-	# ["G12nG22",set({1,2,3,4}),False],
-	# ["G11nG22",set({1,2,3,4}),False],
-	# ["G12nG21",set({1,2,3,4}),False],
-
-	# ["G11uG12",set({1,2,3,4}),False],
-	# ["G21uG22",set({1,2,3,4}),False]
 	# ]
-
 
 	# Cstr=[
-	# ["cardeq","G11","2"],
-	# ["cardeq","G12","2"],
-	# ["cardeq","G21","2"],
-	# ["cardeq","G22","2"],
 
-	# ["union","G11","G12","G11uG12"],
-	# ["union","G21","G22","G21uG22"],
-
-	# ["equals","G11uG12","P"],
-	# ["equals","G21uG22","P"],
-
-	# ["intersect","G11","G12","G11nG12"],
-	# ["intersect","G11","G21","G11nG21"],
-	# ["intersect","G21","G22","G21nG22"],
-	# ["intersect","G12","G22","G12nG22"],
-	# ["intersect","G11","G22","G11nG22"],
-	# ["intersect","G12","G21","G12nG21"],
-
-	# ["cardlt","G11nG12","2"],
-	# ["cardlt","G11nG21","2"],
-	# ["cardlt","G21nG22","2"],
-	# ["cardlt","G12nG22","2"],
-	# ["cardlt","G12nG21","2"],
-	# ["cardlt","G11nG22","2"]
+	# 	["equals","J","E"],
+	# 	["cardlt","O","3"],
+	# 	["existsIn","2","O"],
 	# ]
+
+
+	Ens=[
+	["P",set({1,2,3,4}),True],
+
+	["2",int(2),True],
+
+	["G11",set({1,2,3,4}),False],
+	["G12",set({1,2,3,4}),False],
+	["G21",set({1,2,3,4}),False],
+	["G22",set({1,2,3,4}),False],
+
+
+	["G11nG12",set({1,2,3,4}),False],
+	["G11nG21",set({1,2,3,4}),False],
+	["G21nG22",set({1,2,3,4}),False],
+	["G12nG22",set({1,2,3,4}),False],
+	["G11nG22",set({1,2,3,4}),False],
+	["G12nG21",set({1,2,3,4}),False],
+
+	["G11uG12",set({1,2,3,4}),False],
+	["G21uG22",set({1,2,3,4}),False]
+	]
+
+
+	Cstr=[
+	["cardeq","G11","2"],
+	["cardeq","G12","2"],
+	["cardeq","G21","2"],
+	["cardeq","G22","2"],
+
+	["union","G11","G12","G11uG12"],
+	["union","G21","G22","G21uG22"],
+
+	["equals","G11uG12","P"],
+	["equals","G21uG22","P"],
+
+	["intersect","G11","G12","G11nG12"],
+	["intersect","G11","G21","G11nG21"],
+	["intersect","G21","G22","G21nG22"],
+	["intersect","G12","G22","G12nG22"],
+	["intersect","G11","G22","G11nG22"],
+	["intersect","G12","G21","G12nG21"],
+
+	["cardlt","G11nG12","2"],
+	["cardlt","G11nG21","2"],
+	["cardlt","G21nG22","2"],
+	["cardlt","G12nG22","2"],
+	["cardlt","G12nG21","2"],
+	["cardlt","G11nG22","2"]
+	]
 
 	start_time = time.time()
 	Launch_Solver(Ens,Cstr,False)
